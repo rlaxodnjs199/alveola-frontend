b4cc422f5f8ec09fa4f9a9038ec3a3249837fba3
Taewon Kim <rlaxodnjs199@gmail.com>
2021-10-08 09:30:30 -0500

    modify sidebar page list

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -1,13 +1,13 @@
 import dashboard from './dashboard';
-import pages from './pages';
-import utilities from './utilities';
+// import pages from './pages';
+// import utilities from './utilities';
 import other from './other';
 import ctscan from './ct-scans';
 
 // ===========================|| MENU ITEMS ||=========================== //
 
 const menuItems = {
-  items: [ctscan, pages, dashboard, utilities, other],
+  items: [ctscan, dashboard, other],
 };
 
 export default menuItems;
